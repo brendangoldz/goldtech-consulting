0ee5c4ee8c1e939cbcae8b20788bb31dd7516963
Brendan Goldsmith <brendangoldz@gmail.com>
2023-11-15 14:51:44 -0500

    Last Touch UI/UX Animations

--- goldtech-consulting/src/components/about/AboutSection.js
+++ goldtech-consulting/src/components/about/AboutSection.js
@@ -1,29 +1,47 @@
+import React, { useState } from 'react';
 import './AboutSection.css';
 import Navigation from '../nav/Navbar';  // Assuming you have Navbar.js in a folder named components
 
 function AboutSection({ scrollTo }) {
-    return (
-      <div className="about-container h-screen flex flex-col justify-center items-center">
-        <Navigation scrollTo={scrollTo}/>
-        <h2>About Us</h2>
-        <section id="about">
-          <div className="about-section">
-            <p>
-              At GoldTech Consulting LLC, we harness the power of technology to drive business transformation. In this digital age, we stand as your trusted ally, guiding you through tech's complexities.
-            </p>
-            <p>
-              Our expertise spans programming, bespoke software solutions, IoT innovations, and effective project management. We're not just service providers; we're your partners in success, committed to crafting solutions that set you apart.
-            </p>
-            <p>
-              In an ever-evolving tech landscape, GoldTech Consulting leads the curve, ready to embark on a transformative journey with you.
-            </p>
-              <a href="#projects" className="cta-button">
-              See Our Projects
-            </a>
-          </div>
-        </section>
-      </div>
-    );
-  }
-  
-  export default AboutSection;
+  const [buttonText, setButtonText] = useState('See Our Projects');
+  const [buttonStyle, setButtonStyle] = useState({});
+
+  const handleClick = () => {
+    setButtonText('Coming Soon!');
+    setButtonStyle({
+      backgroundColor: '#c7c7c7', // Background color of the section
+      border: '2px solid #ffde59' // Border with original button color
+    });
+
+    setTimeout(() => {
+      setButtonText('See Our Projects');
+      setButtonStyle({});
+    }, 3000); // Revert back after 3 seconds
+  };
+
+  return (
+    <div className="about-container h-screen flex flex-col justify-center items-center">
+      <Navigation scrollTo={scrollTo} />
+      <h2>About Us</h2>
+      <section id="about">
+        <div className="about-section">
+          <p>
+            At GoldTech Consulting LLC, we harness the power of technology to drive business transformation. In this digital age, we stand as your trusted ally, guiding you through tech's complexities.
+          </p>
+          <p>
+            Our expertise spans programming, bespoke software solutions, IoT innovations, and effective project management. We're not just service providers; we're your partners in success, committed to crafting solutions that set you apart.
+          </p>
+          <p>
+            In an ever-evolving tech landscape, GoldTech Consulting leads the curve, ready to embark on a transformative journey with you.
+          </p>
+          <button className="cta-button" style={buttonStyle} onClick={handleClick}>
+            {buttonText}
+          </button>
+
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default AboutSection;
--- goldtech-consulting/src/components/contact/ContactSection.js
+++ goldtech-consulting/src/components/contact/ContactSection.js
@@ -1,73 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ContactSection.css'
-import Navigation from '../nav/Navbar'; 
+import Navigation from '../nav/Navbar';
 import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import { FaCheck } from 'react-icons/fa'; // Importing check icon from react-icons
 
 function ContactSection({ scrollTo }) {
-  const handleSubmit = async (event) => {
-      event.preventDefault();
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-      const email = event.target.formBasicEmail.value;
-      const subject = event.target.formBasicSubject.value;
-      const message = event.target.formBasicMessage.value;
+        const email = event.target.formBasicEmail.value;
+        const subject = event.target.formBasicSubject.value;
+        const message = event.target.formBasicMessage.value;
 
-      try {
-          const response = await axios.post(
-              'https://3myxbr87g6.execute-api.us-east-2.amazonaws.com/contact/',
-              {
-                  "email": email,
-                  "subject": subject,
-                  "message": message
-              },
-              {
-                  headers: {
-                    'x-api-key': 'I3z5vv3C7K4IWQxrWbPeC334cGo9fjQo4KEjUE2D',
-                    "Content-Type": "application/json"
-                  }
-              }
-          );
+        try {
+            const response = await axios.post(
+                'https://3myxbr87g6.execute-api.us-east-2.amazonaws.com/contact/',
+                {
+                    "email": email,
+                    "subject": subject,
+                    "message": message
+                },
+                {
+                    headers: {
+                        'x-api-key': 'I3z5vv3C7K4IWQxrWbPeC334cGo9fjQo4KEjUE2D',
+                        "Content-Type": "application/json"
+                    }
+                }
+            );
 
-          if (response.data.message) {
-              console.log(response.data.message); // or display it on the page
-          }
-      } catch (error) {
-          console.error('Failed to send message:', error.response); // handle error appropriately in UI
-      }
+            if (response.data.message) {
+                console.log(response.data.message); // or display it on the page
+                setIsSubmitted(true);
+            }
+            setTimeout(() => {
+                setIsSubmitted(false); // Optionally reset after a certain time
+            }, 3000);
+        } catch (error) {
+            console.error('Failed to send message:', error.response); // handle error appropriately in UI
+        }
     };
 
     return (
-      <div className="contact-container h-screen flex flex-col justify-center items-center">
-        <Navigation scrollTo={scrollTo}/>
-        <h2>Contact Form</h2>
-        <section id="contact">
-          <div className="contact-section">
-            <Form  onSubmit={handleSubmit}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
+        <div className="contact-container h-screen flex flex-col justify-center items-center">
+            <Navigation scrollTo={scrollTo} />
+            <h2>Contact Form</h2>
+            <section id="contact">
+                <div className="contact-section">
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                                We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
 
-                <Form.Group controlId="formBasicSubject">
-                    <Form.Label>Subject</Form.Label>
-                    <Form.Control type="text" placeholder="Subject" />
-                </Form.Group>
+                        <Form.Group controlId="formBasicSubject">
+                            <Form.Label>Subject</Form.Label>
+                            <Form.Control type="text" placeholder="Subject" />
+                        </Form.Group>
 
-                <Form.Group controlId="formBasicMessage">
-                    <Form.Label>Message</Form.Label>
-                    <Form.Control as="textarea" rows={3} placeholder="Your message..." />
-                </Form.Group>
-                
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-          </div>
-        </section>
-      </div>
+                        <Form.Group controlId="formBasicMessage">
+                            <Form.Label>Message</Form.Label>
+                            <Form.Control as="textarea" rows={3} placeholder="Your message..." />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit" className={isSubmitted ? 'submit-success' : ''}>
+                            {isSubmitted ? <FaCheck className="checkmark"/> : 'Submit'}
+                        </Button>
+                    </Form>
+                </div>
+            </section>
+        </div>
     );
 }
 
