1acb2691fe507b303d7626c0b4ddbaa2e1d8055d
Brendan Goldsmith <brendangoldz@gmail.com>
2026-01-14 19:10:44 -0500

    Tailwind Background Color Fixes

--- goldtech-consulting/tailwind.config.js
+++ goldtech-consulting/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  safelist: [
+    'bg-marketing-bg/60',
+    'bg-white/70',
+  ],
   theme: {
     extend: {
       fontFamily: {
